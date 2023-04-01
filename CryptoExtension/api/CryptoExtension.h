aa4148a77eb7cc245d00ce973fc1242f0517fdca
stupid cat <cat@blargbot.xyz>
2023-04-01 13:43:52 -0600

    blah

---
+++ CryptoExtension/api/CryptoExtension.h
@@ -0,0 +1,19 @@
+﻿// CryptoExtension.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#ifdef _WIN32
+#define GMDLL extern "C" __declspec(dllexport)
+#else
+#define GMDLL
+#endif
+
+// TODO: Reference additional headers your program requires here.
+
+namespace CryptoExtension {
+
+    GMDLL double DLLAddNumbers(double a, double b);
+    GMDLL char* DLLPrintSomething();
+
+}
---
+++ CryptoExtension/src/CryptoExtension.cpp
@@ -0,0 +1,22 @@
+﻿// CryptoExtension.cpp : Defines the entry point for the application.
+//
+
+#include "../api/CryptoExtension.h"
+
+#ifdef _WIN32
+#define GMDLL extern "C" __declspec(dllexport)
+#else
+#define GMDLL
+#endif
+
+namespace CryptoExtension {
+
+    GMDLL double DLLAddNumbers(double a, double b) {
+        return a + b;
+    }
+
+    GMDLL char* DLLPrintSomething() {
+        return (char*)"testing.";
+    }
+
+}
