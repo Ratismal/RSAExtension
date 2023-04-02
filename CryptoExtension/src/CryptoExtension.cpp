0013c26d070bc9815cb6ef453668a7bfb36d0736
stupid cat <cat@blargbot.xyz>
2023-04-02 13:13:48 -0600

    cleanup

--- CryptoExtension/src/CryptoExtension.cpp
+++ CryptoExtension/src/CryptoExtension.cpp
@@ -9,15 +9,7 @@ using namespace CryptoPP;
 using namespace std;
 
 namespace CryptoExtension {
-
-    GMDLL double DLLAddNumbers(double a, double b) {
-        return a + b;
-    }
-
-    GMDLL char* DLLPrintSomething() {
-        return (char*)"testing.";
-    }
-
+    // Generate the key components and outputs a set of macros to console that can be used within GMS2
     GMDLL char* DLLGenerateKeys() {
         ///////////////////////////////////////
         // Pseudo Random Number Generator
@@ -46,11 +38,6 @@ namespace CryptoExtension {
         cout << "#macro RSA_E \"" << e << "\"" << endl;
         cout << endl;
 
-        ///////////////////////////////////////
-        // Create Keys
-        RSA::PrivateKey privateKey(params);
-        RSA::PublicKey publicKey(params);
-
         return (char*)"wow!";
     }
 
@@ -58,42 +45,10 @@ namespace CryptoExtension {
         char* array = new char[input.length() + 1];
         input.copy(array, input.length() + 1);
         return array;
-        /*
-        const int length = input.length();
-        char* char_array = new char[int(length) + 1];
-
-        // copying the contents of the
-        // string to char array
-        strcpy(char_array, input.c_str());
-        return (char*)char_array;
-        */
     }
 
-    GMDLL char* DLLRSASign(char* input, char* _n, char* _e, char* _d) {
-        AutoSeededRandomPool rng;
-
-        Integer n(_n), e(_e), d(_d);
-
-        RSA::PrivateKey privKey;
-        privKey.Initialize(n, e, d);
-
-        RSA::PublicKey pubKey;
-        pubKey.Initialize(n, e);
-
-        string message(input), signature;
-
-        // Sign and Encode
-        RSASSA_PKCS1v15_SHA_Signer signer(privKey);
-
-        StringSource ss1(message, true,
-            new SignerFilter(rng, signer,
-                new StringSink(signature)
-            ) // SignerFilter
-        ); // StringSource
-
-        return toCharArray(signature);
-    }
-
+    // GMS2 forces you to provide all args as the same data type when you have more than 4, for some reason.
+    // So to cut down on arguments, we can pass all the key components as a single string, pipe delimited
     vector<char*> tokenify(char* input, char* delimiter) {
         vector<char*> v;
 
@@ -106,7 +61,7 @@ namespace CryptoExtension {
         return v;
     }
 
-    char const hex[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A',   'B','C','D','E','F' };
+    char const hex[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B','C','D','E','F' };
 
     std::string charToHex(char const ch) {
         std::string str;
@@ -115,10 +70,10 @@ namespace CryptoExtension {
         return str;
     }
 
-    std::string byte_2_str(char* bytes, int size) {
+    std::string charsToHex(string bytes) {
         std::string str;
-        for (int i = 0; i < size; ++i) {
-            const char ch = bytes[i];
+        for (int i = 0; i < bytes.length(); ++i) {
+            const char ch = bytes.at(i);
             str.append(&hex[(ch & 0xF0) >> 4], 1);
             str.append(&hex[ch & 0xF], 1);
             str.append(" ");
@@ -129,6 +84,12 @@ namespace CryptoExtension {
     vector<byte> convertToBytes(const int* buffer, double length) {
         vector<byte> bytes;
 
+        /* 
+            The buffer provided has the granularity of bytes, however we can only iterate it in ints.
+            To account for this, the buffer is padded.
+            The provided length is only for the actual bytes we care about, not including padding,
+            so we need to increase it to a limit that makes sure we iterate through all the ints.
+        */
         int byteCount = 0;
         int limit = length / 4;
         int mod = int(length) % 4;
@@ -139,19 +100,18 @@ namespace CryptoExtension {
         for (int i = 0; i < limit; i++) {
             unsigned char b[4];
             unsigned long n = buffer[i];
-            b[0] = (n >> 24) & 0xFF;
-            b[1] = (n >> 16) & 0xFF;
-            b[2] = (n >> 8) & 0xFF;
-            b[3] = n & 0xFF;
+            b[3] = (n >> 24) & 0xFF;
+            b[2] = (n >> 16) & 0xFF;
+            b[1] = (n >> 8) & 0xFF;
+            b[0] = n & 0xFF;
 
             for (int j = 0; j < 4; j++) {
-                int index = 3 - j;
-
                 bytes.push_back(b[j]);
 
-                cout << "Added byte " << byteCount << " " <<  charToHex(b[3 - j]) << " " << b[3 - j] << endl;
+                // cout << "Added byte " << byteCount << " " <<  charToHex(b[j]) << " " << b[j] << endl;
 
                 if (++byteCount >= length) {
+                    // We've reached the last byte before the padding, so we're done.
                     break;
                 }
             }
@@ -184,13 +144,15 @@ namespace CryptoExtension {
             ) // SignerFilter
         ); // StringSource
 
-        return toCharArray(byte_2_str(toCharArray(signature), signature.length()));
+        // Awful Hack
+        // We can't return the string as-is because it may contain null bytes, which will
+        // cause the data to be truncated. So convert the bytes to hex strings and parse
+        // on GMS2's side.
+        return toCharArray(charsToHex(signature));
     }
 
     GMDLL double DLLRSAVerifyBuffer(const int* buffer, double length, char* pub) {
         vector<char*> tokens = tokenify(pub, "|");
-
-        // cout << "Starting verify." << endl;
 
         AutoSeededRandomPool rng;
 
@@ -199,36 +161,11 @@ namespace CryptoExtension {
         RSA::PublicKey pubKey;
         pubKey.Initialize(n, e);
 
-        // cout << "finished making keys" << endl;
-
-
         // Verify and Recover
         RSASSA_PKCS1v15_SHA_Verifier verifier(pubKey);
 
-        // cout << "verifying." << endl;
-
         try {
-            // cout << "h." << endl;
-
             vector<byte> bytes = convertToBytes(buffer, length);
-
-            /*
-            byte* messageBytes = new byte[length - 384];
-            byte* signatureBytes = new byte[384];
-
-            int offset = length - 384;
-
-            for (int i = 0; i < length; i++) {
-                if (i < offset) {
-                    messageBytes[i] = bytes[i];
-                    cout << "Added message byte " << i << " " << charToHex(bytes[i]) << " " << bytes[i] << endl;
-                }
-                else {
-                    signatureBytes[i - offset] = bytes[i];
-                    cout << "Added signature byte " << i - offset << " " << charToHex(bytes[i]) << " " << bytes[i] << endl;
-                }
-            }
-            */
 
             VectorSource ss2(bytes, true,
                 new SignatureVerificationFilter(
@@ -236,42 +173,11 @@ namespace CryptoExtension {
                     SignatureVerificationFilter::THROW_EXCEPTION
                 ) // SignatureVerificationFilter
             ); // StringSource
-
-            // cout << "hh." << endl;
         }
         catch (SignatureVerificationFilter::SignatureVerificationFailed err) {
-            // cout << "error: " << err.what() << endl;
-            
-
             return 0;
         }
 
-        // cout << "done." << endl;
-
         return 1;
     }
-
-    GMDLL double DLLRSAVerify(char* input, char* signature, char* _n, char* _e) {
-        AutoSeededRandomPool rng;
-
-        Integer n(_n), e(_e);
-
-        RSA::PublicKey pubKey;
-        pubKey.Initialize(n, e);
-
-        string message(input);
-
-        // Verify and Recover
-        RSASSA_PKCS1v15_SHA_Verifier verifier(pubKey);
-
-        StringSource ss2(message, true,
-            new SignatureVerificationFilter(
-                verifier, NULL,
-                SignatureVerificationFilter::THROW_EXCEPTION
-            ) // SignatureVerificationFilter
-        ); // StringSource
-
-        return 1;
-    }
-
 }
